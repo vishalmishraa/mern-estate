3cd9d55257202b6f685560a4a124b0f144b17ef7
vishal mishra <vishal.mishra9470@gmail.com>
2024-01-17 12:28:20 +0530

    Add  mongoose dependencies, and establish MongoDB connection

--- v1/api/index.js
+++ v1/api/index.js
@@ -1,6 +1,27 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
-const app = express();
+import mongoose from 'mongoose';
 
+
+/**************** MONGO DB CONNECTION *********************** */
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+let db = async () => {
+    await mongoose.connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("database connected");
+    }).catch(err => {
+        console.log('ERROR', err.message);
+    });//connecting mongo -> THE NAME OF WHAT WE WILL LOOK FOR IN MONGO
+
+}
+db();
+
+
+const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
