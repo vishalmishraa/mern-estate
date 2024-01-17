371a4fcce5ed5b55ae3d344da9213ef66acb3b72
vishal mishra <vishal.mishra9470@gmail.com>
2024-01-17 12:35:14 +0530

    Add user model definition

---
+++ v1/api/models/user.model.js
@@ -0,0 +1,24 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    
+},{timestamps:true});
+
+const User = mongoose.model('User',userSchema);
+export default User;
