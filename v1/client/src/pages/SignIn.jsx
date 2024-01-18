86c2469d8f79001011e3d3e05c396515f61aa555
vishal mishra <vishal.mishra9470@gmail.com>
2024-01-18 17:51:40 +0530

    Update Sign In button label

--- v1/client/src/pages/SignIn.jsx
+++ v1/client/src/pages/SignIn.jsx
@@ -53,7 +53,7 @@ export default function SignIp() {
       <form onSubmit={handleSubmit} action="" className='flex flex-col gap-4 '>
           <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'onChange = {handleChange} />
           <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password'onChange = {handleChange} />
-          <button disabled = {loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">{loading ? "Loading..." : "Sign Up"}</button>
+          <button disabled = {loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">{loading ? "Loading..." : "Sign In"}</button>
           <Oauth></Oauth>
       </form>
 
