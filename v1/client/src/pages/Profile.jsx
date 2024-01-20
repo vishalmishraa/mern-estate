1b86d3d04325eb8b87b4ae7e145f5f9536fe1767
vishal mishra <vishal.mishra9470@gmail.com>
2024-01-20 16:14:30 +0530

    Add Profile component and add user profile form

--- v1/client/src/pages/Profile.jsx
+++ v1/client/src/pages/Profile.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import {useSelector} from 'react-redux' 
 
-export default function profile() {
-  return (
-    <div>profile</div>
+export default function Profile() {
+    const {currentUser} = useSelector(state => state.user)
+
+    return (
+    <div className='p-3 max-w-lg mx-auto'>
+       <h1 
+        className="text-3xl font-semibold text-center my-7 "
+       >profile</h1>
+        <form action="" className='flex flex-col gap-4'>
+            <img src={ currentUser.avatar } alt="profile"  className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+            <input type="text" name="" id="username" placeholder='username' className='border p-3 rounded-lg' />
+            <input type="email" name="" id="email" placeholder='email' className='border p-3 rounded-lg' />
+            <input type="text" name="" id="password" placeholder='password' className='border p-3 rounded-lg' />
+            <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80 '>upate</button>
+        </form>
+        <div className='text-red-700 cursor-pointer flex justify-between mt-5 '>
+            <span>Delete Account</span>
+            <span>Sign Out</span>
+        </div>
+
+
+    </div>
+   
   )
 }
