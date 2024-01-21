aa6a6db8586aaab35182448eb717a5e0cd250fcb
vishal mishra <vishal.mishra9470@gmail.com>
2024-01-21 11:23:28 +0530

    Remove unnecessary import statement in auth.routes.js

--- v1/api/routes/auth.routes.js
+++ v1/api/routes/auth.routes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { google, signin, signup } from '../controllers/auth.controllers.js';
-import exp from 'constants';
 const router = express.Router();
 
 router.post('/signup',signup);
