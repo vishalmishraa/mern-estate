12d399fe324b28770e147eaf560c81d9fda06ec1
vishal mishra <vishal.mishra9470@gmail.com>
2024-01-26 23:18:01 +0530

    Complete About page content

--- v1/client/src/pages/About.jsx
+++ v1/client/src/pages/About.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800
+      '>About FYDH</h1>
+
+      <p className='mb-4 text-slate-700 '>
+          FYDH is a reputable real estate company that specializes in providing exceptional services and solutions
+           in the real estate industry. With a team of experienced professionals, FYDH is committed to delivering 
+           innovative and cutting-edge solutions to meet the diverse needs of its clients. Whether its buying, 
+           selling, or renting properties, FYDH offers a wide range of services to cater to the unique requirements
+            of individuals and businesses.
+        </p>
+        <p className='mb-4 text-slate-700 '>
+          At FYDH, we understand the importance of finding the perfect property that meets your specific criteria.
+           Our dedicated team of real estate experts is well-equipped with market knowledge and expertise to guide you
+            through the entire process, ensuring a seamless and hassle-free experience. We prioritize customer 
+            satisfaction and strive to build long-term relationships with our clients based on trust and transparency.
+        </p>
+        <p className='mb-4 text-slate-700 '>
+         With a strong network and extensive industry connections, FYDH has access to a wide range of properties, 
+          including residential, commercial, and investment opportunities. We stay up-to-date with the latest market
+            trends and leverage technology to provide accurate and timely information to our clients.Whether you are a
+          first-time homebuyer, an investor, or a business owner looking for commercial space, FYDH is your trusted
+           partner in the real estate market. Contact us today to explore the possibilities and let us help you find
+            your dream property.
+        </p>
+    </div>
   )
 }
