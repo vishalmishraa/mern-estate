b10d3049398455844475d2f3d2822d5baabe245e
vishal mishra <vishal.mishra9470@gmail.com>
2024-01-27 12:04:27 +0530

    Fix avatar source in Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -65,7 +65,7 @@ export default function Header() {
             {currentUser ? (
               <img
                 className='rounded-full h-7 w-7 object-cover'
-                src={currentUser.avatar}
+                src={currentUser.avatar || currentUser.data.avatar}
                 alt='profile'
               />
             ) : (
