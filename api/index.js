c3185b6ffc47ff84ecca9d74bc38db8f116edd3d
vishal mishra <vishal.mishra9470@gmail.com>
2024-01-28 16:52:34 +0530

    Update server port to use environment variable

--- api/index.js
+++ api/index.js
@@ -50,6 +50,6 @@ app.use((err,req,res,next)=>{
 });
 
 /* app routes */
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server is running on: http://localhost:3000');
 });
