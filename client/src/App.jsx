fd5149d2cc583a9cfd9ed33e2d2fdf658d1de5e1
vishal mishra <vishal.mishra9470@gmail.com>
2024-02-12 17:24:24 +0530

    Refactor import statements in App.jsx and create index.js in pages directory

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,42 @@
-import {BrowserRouter , Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import SignUp from './pages/SignUp'
-import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PrivateRoute, { UserLogedIn } from './components/PrivateRoute'
-import CreateListing from './pages/CreateListing'
-import UpdateListing from './pages/UpdateListing'
-import Listing from './pages/Listing'
-import Search from './pages/Search'
-
+import {
+  Home,
+  SignIn,
+  About,
+  Profile,
+  SignUp,
+  Header,
+  UserLogedIn,
+  CreateListing,
+  UpdateListing,
+  Listing,
+  Search
+} from './pages/index.js';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/search' element={<Search/>}/>
-        <Route path='/listing/:listingId' element={<Listing/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
 
-        <Route  element={<UserLogedIn/>}>
-            <Route path='/sign-in' element={<SignIn/>}/>
-            <Route path='/sign-up' element={<SignUp/>}/>
+        <Route element={<UserLogedIn />}>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
         </Route>
 
 
         <Route element={<PrivateRoute></PrivateRoute>} >
-            <Route path='/profile' element={<Profile/>}/>
-            <Route path='/create-listing' element={<CreateListing/>}></Route>
-            <Route path='/update-listing/:listingId' element={<UpdateListing/>}></Route>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />}></Route>
+          <Route path='/update-listing/:listingId' element={<UpdateListing />}></Route>
         </Route>
-  
+
 
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/index.js
@@ -0,0 +1,24 @@
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+import CreateListing from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
+import Search from './pages/Search'
+
+export {
+  Home,
+  SignIn,
+  About,
+  Profile,
+  SignUp,
+  Header,
+  UserLogedIn,
+  CreateListing,
+  UpdateListing,
+  Listing,
+  Search
+};
