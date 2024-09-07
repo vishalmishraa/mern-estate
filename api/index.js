fa02993cbb238d59d7e7dae325abf3706f959c08
vishalmishraa <vishal.mishra9470@gmail.com>
2024-09-07 12:04:36 +0530

    vercel deployment

--- api/index.js
+++ api/index.js
@@ -50,6 +50,8 @@ app.use((err,req,res,next)=>{
 });
 
 /* app routes */
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on: http://localhost:3000');
-});
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log('Server is running on: http://localhost:3000');
+// });
+
+export default app;
